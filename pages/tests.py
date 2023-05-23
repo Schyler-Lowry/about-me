3fc92a695e0848546cb4396f75986b84bdffa1e1
Schyler Lowry <schyler_lowry@outlook.com>
2023-05-23 12:41:05 -0400

    Add tests for Home page, and Contact page. Tests check: good response code, proper template loads returns correct html page template content loads

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomepageTest(SimpleTestCase):
+    """Methods for testing the Home page"""
+    
+    def test_url_exists_at_correct_location(self):
+        """tests that url exists at the correct location"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        """tests that url is available by name"""
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        """tests that the correct template name is loaded"""
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        """tests that the page loads the template's content"""
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1 class=\"mt-5\">Home/About Me</h1>")
+
+class ContactpageTest(SimpleTestCase):
+    """Methods for testing the Contact page"""
+    
+    def test_url_exists_at_correct_location(self):
+        """tests that url exists at the correct location"""
+        response = self.client.get("/contact/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        """tests that url is available by name"""
+        response = self.client.get(reverse("contact"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        """tests that the correct template name is loaded"""
+        response = self.client.get(reverse("contact"))
+        self.assertTemplateUsed(response, "contact.html")
+
+    def test_template_content(self):
+        """tests that the page loads the template's content"""
+        response = self.client.get(reverse("contact"))
+        self.assertContains(response, "<h1 class=\"mt-5\">Contact Me</h1>")
