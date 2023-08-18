4f0cfca2d4a76a44c8619b1d3727359cc50b0940
Schyler Lowry <schyler_lowry@outlook.com>
2023-08-18 15:07:11 -0400

    Add view to render API request. --anime_main.html now shows the top 4 anime from MyAnimeList. Update footer. Add "code" to context data if it passes 'if' check in homepageview.

---
+++ maldata.py
@@ -0,0 +1,19 @@
+{'data': [
+    {'node': {
+        'id': 5114, 
+        'title': 'Fullmetal Alchemist: Brotherhood', 
+        'main_picture': {
+            'medium': 'https://cdn.myanimelist.net/images/anime/1208/94745.jpg', 
+            'large': 'https://cdn.myanimelist.net/images/anime/1208/94745l.jpg'}
+            }, 
+        'ranking': {'rank': 1}
+        }, 
+    {'node': {'id': 9253, 'title': 'Steins;Gate', 'main_picture': {'medium': 'https://cdn.myanimelist.net/images/anime/1935/127974.jpg', 'large': 'https://cdn.myanimelist.net/images/anime/1935/127974l.jpg'}}, 'ranking': {'rank': 2}}, 
+    {'node': {'id': 41467, 'title': 'Bleach: Sennen Kessen-hen', 'main_picture': {'medium': 'https://cdn.myanimelist.net/images/anime/1908/135431.jpg', 'large': 'https://cdn.myanimelist.net/images/anime/1908/135431l.jpg'}}, 'ranking': {'rank': 3}}, 
+    {'node': {'id': 28977, 'title': 'Gintama°', 'main_picture': {'medium': 'https://cdn.myanimelist.net/images/anime/3/72078.jpg', 'large': 'https://cdn.myanimelist.net/images/anime/3/72078l.jpg'}}, 'ranking': {'rank': 4}}
+    ], 
+    'paging': {'next': 'https://api.myanimelist.net/v2/anime/ranking?offset=4&ranking_type=all&limit=4'}
+}
+
+
+
--- pages/views.py
+++ pages/views.py
@@ -4,12 +4,20 @@
 
 from django.views.generic import TemplateView
 
+import ast
+
 import requests
 
 """Using the class method to render pages from the corresponding templates."""
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        if "code" in self.kwargs:
+            context["code"] = self.kwargs["code"]
+        return context
 
 class ProjectsPageView(TemplateView):
     template_name = "projects.html"
@@ -19,11 +27,23 @@ class ContactPageView(TemplateView):
 
 class AnimeMainPageView(TemplateView):
     template_name = "anime_main.html"
-    something = requests.get("https://httpbin.org/status/200")
+    #something = requests.get("https://httpbin.org/status/200")
+
+    header = {
+        "X-MAL-CLIENT-ID": "5a0f93541f97d27dc90508a793000126"
+    }
+
+    #anime = requests.get("https://api.myanimelist.net/v2/anime/30230?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity", headers=header)
+    anime = requests.get("https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=4", headers=header)
+    anime_dict = anime.json()
+    #anime_dict = ast.literal_eval(anime.text)
+    
+    print("My type is:", type(anime_dict))
+    
     
     def get_context_data(self, **kwargs):
         context = super(AnimeMainPageView, self).get_context_data(**kwargs)
-        context["something"] = self.something
+        context["anime"] = self.anime_dict
         return context
     
 
